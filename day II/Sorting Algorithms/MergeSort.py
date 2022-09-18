13cee42b3b57d73ab7f46fe069e3044d7f9da3b8
Shan-Lun-1 <S10222855@connect.np.edu.sg>
2022-09-19 06:25:38 +0800

    Modified documentation for merge sort

--- day II/Sorting Algorithms/MergeSort.py
+++ day II/Sorting Algorithms/MergeSort.py
@@ -23,9 +23,11 @@ def mergeSort(array):
 
         # while both half of arrays contain elements
         while i < len(left) and j < len(right):
+
             # if the current element in the left array is less than the current element in the right array,
             # replace the first element in the unsorted part of the original array (array[k]) with the current element in the left array left[i]
             # else replace the first element in the unsorted part of the original array (array[k]) with the current element in the right array right[j]
+
             if left[i] < right[j]:
                 array[k] = left[i]
                 i += 1
@@ -34,16 +36,16 @@ def mergeSort(array):
                 j += 1
             k += 1
 
-        # after the previous while loop, elements in one half of the original array has all been used
-        # thus, we would now replace the elements in unsorted part of array, with the remaining elements in the other half of the array
+        # after the previous while loop, there might still be remaining elements in one half of the array
+        # thus, we will be replacing the elements in the unsorted part of the orignal array with these remaining elements
 
-        # if there are remaining elements in the left half of the array
+        # while there are remaining elements in the left half of the array, replace the elements in the unsorted part of the orignal array with these remaining elements
         while i < len(left):
             array[k] = left[i]
             i += 1
             k += 1
 
-        # if there are remaining elements in the right half of the array
+        # while there are remaining elements in the right half of the array, replace the elements in the unsorted part of the orignal array with these remaining elements
         while j < len(right):
             array[k] = right[j]
             j += 1
